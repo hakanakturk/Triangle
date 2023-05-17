7a32eec7a781d87f15449cc0fd0340ca281d5454
Hakan Aktürk <hakanakturk@outlook.com>
2023-05-17 14:32:16 +0300

    initial commit

---
+++ Triangle/TriangleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TriangleApp.swift
+//  Triangle
+//
+//  Created by Hakan Aktürk on 17.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct TriangleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
